4abed73a0a0b38332b0692c5678d5682d0decb07
Bruno de Jesus Santos <bru_blink@hotmail.com>
2018-01-09 12:07:56 -0200

    Created the screen for user registration and sending e-mail

---
+++ db.js
@@ -0,0 +1,8 @@
+function createUser(username, password, email, callback){
+    const cryptPwd = bcrypt.hashSync(password, 10)
+    global.db.collection("users").insert({ username, password: cryptPwd, email }, function(err, result){
+        callback(err, result)
+    })
+}
+ 
+module.exports = { createUser }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+ 
+router.get('/signup', function(req, res, next) {
+  if(req.query.fail)
+    res.render('signup', { message: 'Falha no cadastro do usuário!' });
+  else
+    res.render('signup', { message: null });
+})
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+const db = require('../db')
+router.post('/signup', function(req, res, next){
+  db.createUser(req.body.username, req.body.password, req.body.email, (err, result) => {
+    if(err) res.redirect('/signup?fail=true')
+    require('../mail')(req.body.email, 'Bem vindo ao nosso chat', 'Olá ' + req.body.username + ', obrigado por se cadastrar!')
+    res.redirect('/')
+  })
+})
+ 
 module.exports = router;
